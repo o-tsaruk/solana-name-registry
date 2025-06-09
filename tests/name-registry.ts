873d1a0692366850aa213c3bce5bb06f4e1e1f6b
o-tsaruk <oleksandr.v.tsaruk@gmail.com>
2025-06-09 03:30:54 +0300

    update initialization test

--- tests/name-registry.ts
+++ tests/name-registry.ts
@@ -1,15 +1,27 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { NameRegistry } from "../target/types/name_registry";
+import { Keypair } from "@solana/web3.js";
 
 describe("name-registry", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.nameRegistry as Program<NameRegistry>;
+  const user = new Keypair();
+  const payer = provider.wallet as anchor.Wallet;
 
-  it("Is initialized!", async () => {
-    const tx = await program.methods.initialize().rpc();
+  it("Initialization", async () => {
+    const tx = await program.methods
+      .initialize()
+      .accounts({
+        user: user.publicKey,
+        payer: payer.publicKey,
+      })
+      .signers([user])
+      .rpc();
+
     console.log("Your transaction signature", tx);
   });
 });
